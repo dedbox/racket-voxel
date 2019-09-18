27122f66fa8a40c8076837c5eef26f715662f96e
Eric Griffis <dedbox@gmail.com>
2019-09-18 16:38:08 -0700

    update package dependencies

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
 (define collection "voxel-engine")
-(define deps '("base"))
-(define build-deps '())
+(define deps '("base" "opengl" "glm" "gui-lib"))
+(define build-deps '("at-exp-lib" "racket-doc" "scribble-lib"))
 (define scribblings '(("scribblings/voxel-engine.scrbl")))
