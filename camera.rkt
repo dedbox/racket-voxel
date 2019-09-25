dd9f191f3b924965dc2c4af921450cf3cbab976a
Eric Griffis <dedbox@gmail.com>
2019-09-25 01:02:39 -0700

    add FPS camera

--- camera.rkt
+++ camera.rkt
@@ -1,25 +1,64 @@
 #lang racket/base
 
-(require glm)
+(require glm
+         racket/struct)
 
 (provide (all-defined-out))
 
-(define UP (vec3 0.0 1.0 0.0))
+(define UP    (vec3 0 1 0))
+(define RIGHT (vec3 1 0 0))
 
-(struct camera (position view-dir)
+(struct camera (position forward up speed view-matrix)
+  #:transparent
   #:mutable
   #:name vx:camera
   #:constructor-name make-camera)
 
 (define (camera)
-  (make-camera (vec3 0 0 0) (vec3 0 0 -1)))
+  (make-camera (vec3 0 0 0) (vec3 0 0 -1) (vec3 0 1 0) 0.05
+               (look-at (vec3 0 0 0) (vec3 0 0 -1) (vec3 0 1 0))))
 
-;; (define (camera-mouse-update! cam new-mouse)
-;;   (define mouse-Δ (vec- (camera-old-mouse cam) new-mouse))
-;;   (define-values (mouse-Δ.x mouse-Δ.y) (apply values (vec->list mouse-Δ)))
-;;   (define new-dir (mat* (mat3 (rotate mouse-Δ.x UP)) (camera-view-dir cam)))
-;;   )
+(define (move-camera-forward! cam)
+  (define-values (position forward up speed view-matrix)
+    (apply values (struct->list cam)))
+  (vec+= position (* speed forward))
+  (update-camera-view-matrix! cam))
 
-(define (camera-view-matrix cam)
-  (define position (camera-position cam))
-  (look-at position (vec+ position (camera-view-dir cam)) UP))
+(define (move-camera-backward! cam)
+  (define-values (position forward up speed view-matrix)
+    (apply values (struct->list cam)))
+  (vec-= position (* speed forward))
+  (update-camera-view-matrix! cam))
+
+(define (strafe-camera-right! cam)
+  (define-values (position forward up speed view-matrix)
+    (apply values (struct->list cam)))
+  (define right (cross up forward))
+  (vec+= position (* speed right))
+  (update-camera-view-matrix! cam))
+
+(define (strafe-camera-left! cam)
+  (define-values (position forward up speed view-matrix)
+    (apply values (struct->list cam)))
+  (define right (cross up forward))
+  (vec-= position (* speed right))
+  (update-camera-view-matrix! cam))
+
+(define (camera-mouse-update! cam Δx Δy)
+  (define-values (position forward up speed view-matrix)
+    (apply values (struct->list cam)))
+  (define angle (vec3 (radians Δx) (radians Δy)))
+  (set-camera-forward! cam (normalize
+                            (* (mat3 (rotate (mat4) (radians Δx) UP))
+                               (mat3 (rotate (mat4) (radians Δy) RIGHT))
+                               forward)))
+  (set-camera-up! cam (normalize
+                       (* (mat3 (rotate (mat4) (radians Δx) UP))
+                          (mat3 (rotate (mat4) (radians Δy) RIGHT))
+                          up)))
+  (update-camera-view-matrix! cam))
+
+(define (update-camera-view-matrix! cam)
+  (define-values (position forward up speed view-matrix)
+    (apply values (struct->list cam)))
+  (mat=! view-matrix (look-at position (+ position forward) UP)))
--- main.rkt
+++ main.rkt
@@ -2,10 +2,13 @@
 
 (require glm
          racket/class
+         racket/struct
          voxel-engine/camera
          voxel-engine/gl-cube
          voxel-engine/gl-canvas
          voxel-engine/gl-drawable)
+
+(define invert-mouse? #t)
 
 (module+ main
   (define frame (new frame% [label "Voxel Engine"]))
@@ -13,6 +16,7 @@
   ;; camera
 
   (define cam (camera))
+  (define sign (if invert-mouse? 1 -1))
 
   ;; canvas
 
@@ -21,22 +25,29 @@
       (super-new)
       (inherit-field active?)
       (inherit deactivate)
+
+      ;; keyboard
       (define/override (on-char event)
         (match* ((send event get-key-code)
                  (send event get-key-release-code))
           [('escape 'press) (deactivate)]
-          ;; [(#\w 'press) (process-camera-key! cam FORWARD  Δ-time)]
-          ;; [(#\s 'press) (process-camera-key! cam BACKWARD Δ-time)]
-          ;; [(#\a 'press) (process-camera-key! cam LEFT     Δ-time)]
-          ;; [(#\d 'press) (process-camera-key! cam RIGHT    Δ-time)]
+          [(#\w 'press) (move-camera-forward! cam)]
+          [(#\s 'press) (move-camera-backward! cam)]
+          [(#\a 'press) (strafe-camera-right! cam)]
+          [(#\d 'press) (strafe-camera-left! cam)]
           [('release _) (void)]
           [(_ _) (super on-char event)]))
-      ;; mouse events
+
+      ;; mouse
       (define/override (on-event event)
         (match* ((send event get-event-type)
                  (send event get-x)
                  (send event get-y))
-          [('motion x y) (println `(mouse motion ,x ,y))]
+          [('motion x y)
+           (define-values (x-mid y-mid) (get-canvas-center))
+           (unless (and (= x x-mid) (= y y-mid))
+             (camera-mouse-update! cam (- x-mid x) (* sign (- y-mid y)))
+             (warp-pointer-to-center))]
           [(_ _ _) (super on-event event)]))))
 
   (define canvas
@@ -52,18 +63,28 @@
 
   ;; timing
 
-  (define Δ-time 0.0)
-  (define last-frame 0.0)
+  ;; (define Δ-time 0.0)
+  ;; (define last-frame 0.0)
 
   ;; GUI
+
+  (define (get-canvas-center)
+    (let-values ([(width height) (send canvas get-gl-client-size)])
+      (values (/ width 2) (/ height 2))))
+
+  (define (warp-pointer-to-center)
+    (let-values ([(x y) (get-canvas-center)]) (send canvas warp-pointer x y)))
 
   (send frame show #t)
   (send canvas focus)
   (send canvas set-cursor (make-object cursor% 'blank))
+  (warp-pointer-to-center)
 
   ;; scene
 
   (define cube (gl-cube canvas))
+
+  (define projection (perspective (radians 80) (send canvas aspect-ratio) 1 15))
 
   ;; main loop
 
@@ -74,48 +95,27 @@
              (let loop ()
                (unless (get-field stopping? canvas)
                  (collect-garbage 'incremental)
+                 (send canvas clear)
 
-                 ;; timing
-                 (define current-frame (current-inexact-milliseconds))
-                 (set! Δ-time (- current-frame last-frame))
-                 (set! last-frame current-frame)
-
-                 ;; scene
-                 (define model
-                   (* (translate (mat4) (vec3 0 0 -10))
-                      (rotate (mat4)
-                              (/ (current-inexact-milliseconds) 500)
-                              (vec3 1 1/2 1/4))
-                      (scale (mat4) (vec3 5))))
-                 (define view (camera-view-matrix cam))
-                 (define projection
-                   (perspective (radians 80) (send canvas aspect-ratio) 2 10))
+                 ;; ;; timing
+                 ;; (define current-frame (current-inexact-milliseconds))
+                 ;; (set! Δ-time (- current-frame last-frame))
+                 ;; (set! last-frame current-frame)
 
                  ;; draw
-                 (send canvas clear)
-                 (gl-draw cube (mat* projection view model))
+                 (for* ([x (in-range 3)]
+                        [y (in-range 3)]
+                        [z (in-range 3)])
+                   (define model (* (translate (mat4) (vec3 (- x 3/2) (- y 3/2) (- z 6)))
+                                    (scale (mat4) (vec3 1/2))))
+                   (gl-draw cube (* projection (transpose (camera-view-matrix cam)) model)))
 
                  ;; commit
                  (send canvas swap-gl-buffers)
                  (sleep)
                  (loop))
 
+               ;; shut down
                (send canvas terminate)
                (exit))))])
     (void t)))
-
-;; (module+ test
-;;   ;; (define cam (camera (vec3 0.0 0.0 3.0)))
-;;   (define model (mat*
-;;                  (translate (mat4 1.0) (vec3 0.0 0.0 -3.0))
-;;                  ;; (rotate (mat4 1.0) (radians 3.0) (vec3 1.0 0.0 0.0))
-;;                  ;; (scale (mat4 1.0) (vec3 0.5))
-;;                  ))
-;;   (define view (mat4 1.0))
-;;   ;; (define view (look-at (vec3 0.0 0.0 1.0)
-;;   ;;                       (vec3 0.0 0.0 0.0)
-;;   ;;                       (vec3 0.0 1.0 0.0)))
-;;   ;; (define projection (ortho -1.0 1.0 -1.0 1.0 -1.0 1.0))
-;;   ;; (define projection (perspective (radians 45.0) (/ 4.0 3.0) 0.1 1.1))
-;;   (define projection (perspective (radians 60.0) (/ 4.0 3.0) 0.1 10.0))
-;;   (println (mat* projection view model)))
